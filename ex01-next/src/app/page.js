0e00626150448af7a86c251a6d0f3c765ba3ba1d
pablo felipe <pabloletiane@gmail.com>
2025-08-19 15:14:06 -0300

    fix+ add:Edição da configuração base next e Criação do novo projeto (mini bio)

--- ex01-next/src/app/page.js
+++ ex01-next/src/app/page.js
@@ -1,15 +1,17 @@
 import React from "react";
 import Head from "@/components/Head";
+import Exemplo from "@/components/Exemplo";
+import "./index.css";
+
 
 export default function Home() {
   return (
     <div>
       <h1>Welcome to my app</h1>
       <Head />
-      <Head />
-      <Head />
-      <Head />
-      <Head />
+      <Exemplo />
+      <p>This is a simple example of a Next.js application.</p>
+      <p>Feel free to explore the code and modify it as you wish!</p>
     </div>
   );
 }
---
+++ ex01-next/src/components/Exemplo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Exemplo = () => {
+  return (
+    <div className ="exemplo" >Exemplo</div>
+  )
+}
+
+export default Exemplo
