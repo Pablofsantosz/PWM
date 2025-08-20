056f52b7076b3a248aa17244a6ec1a4ef60dba99
pablo felipe <pabloletiane@gmail.com>
2025-08-20 19:08:05 -0300

    Correção no <P/>:  O Erro do Apóstrofo , o apóstrofo nao é aceito pelo vercel(erro de build)

--- mini-bio/src/app/page.js
+++ mini-bio/src/app/page.js
@@ -5,7 +5,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <>
-    <div className='main'>
+    <div className="main">
     <Image
     src="/images/MinhaFoto.jpg"
     alt="Minha Foto"
@@ -15,7 +15,7 @@ export default function Home() {
     {/*<img src="/images/MinhaFoto.jpg" alt="Logo" className='minha-foto' /> isso da problema na hora de dar deploy(Vercel) */}
 
     <h1 >Pablo Felipe dos Santos</h1>
-    <p>Studying a Bachelor's Degree in Computer Science (5th semester) at the Catholic University of Pernambuco (UNICAP). Passionate about software development and quality assurance, I'm always looking to improve my skills and contribute to meaningful projects. I develop using <strong>PythonPython, C#, JavaScript, C, Java, </strong>and use libraries and frameworks like React, Django, and Node.js to build impactful projects.</p>
+    <p>{"Studying a Bachelor's Degree in Computer Science (5th semester) at the Catholic University of Pernambuco (UNICAP). Passionate about software development and quality assurance, I'm always looking to improve my skills and contribute to meaningful projects. I develop using <strong>Python, C#, JavaScript, C, Java,</strong> and use libraries and frameworks like React, Django, and Node.js to build impactful projects."}</p>
 
    </div>
   </>
